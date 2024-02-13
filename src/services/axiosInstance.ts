66701d1cd6aaf6dd4628627a7e43046ca9f417c7
muhammad abdelhay <abdelhay.courses@gmail.com>
2024-02-13 12:52:49 +0200

    add .env file

--- src/services/axiosInstance.ts
+++ src/services/axiosInstance.ts
@@ -3,7 +3,7 @@ import {toastService} from "./toastService.ts";
 import {logout} from "./logout.ts";
 
 // Define your API base URL
-const BASE_URL = 'http://localhost:8888';
+const BASE_URL = import.meta.env.BACKEND_URL;
 
 // Create an Axios instance
 const axiosInstance: AxiosInstance = axios.create({
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import {VitePWA} from 'vite-plugin-pwa'
+import dotenv from "dotenv";
 
 export default defineConfig({
     plugins: [
         react(),
         VitePWA()
-    ]
+    ],
+    define: {
+        'process.env.BACKEND_URL': JSON.stringify(process.env.BACKEND_URL)
+    }
 })
